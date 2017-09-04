2807975d43359624c6cf8eb590ec21043d7ed083
Eddie <Eddie@Edwins-MacBook-Pro.local>
2017-09-04 13:13:22 -0700

    Wrote test for expense reducer

--- lab-eddie/src/__test__/expense-reducer.test.js
+++ lab-eddie/src/__test__/expense-reducer.test.js
@@ -5,12 +5,13 @@ describe('Expense Reducer Test', () => {
     1234: [
       {
         title: 'milk',
-        buget: 5,
+        budget: 5,
         published: new Date(),
         id: 666,
         categoryID: 1234
       }
     ]}
+
   test('Init State should be an empty object literal', () => {
     let testResult = expenseReducer({}, {type: null});
     expect(testResult).toEqual({});
@@ -27,28 +28,52 @@ describe('Expense Reducer Test', () => {
     expect(testResult[1234].length).toEqual(0);
     expect(testResult[1234][0]).toBe(undefined);
   });
-//   // test('Should return a state with a new expense added', () => {
-//   //   let newExpense = {title: 'milk', budget: 3, id: 123}
-//   //   let target = {type: 'EXPENSE_CREATE', payload: newExpense}
-//   //   let testResult = expenseReducer(state, target);
-//   //   expect(testResult.length).toEqual(2);
-//   //   expect(testResult[0]).toBe(state[0]);
-//   //   expect(testResult[1]).toEqual(newExpense);
-//   // });
-//   // test('Should return a state with a modified target', () => {
-//   //   let modExpense = state[1]
-//   //   modExpense.title = 'cake'
-//   //   let target = {type: 'EXPENSE_UPDATE', payload: modExpense}
-//   //   let testResult = expenseReducer(state, target);
-//   //   expect(testResult.length).toEqual(2);
-//   //   expect(testResult[0]).toBe(state[0]);
-//   //   expect(testResult[1]).toEqual(modExpense);
-//   // });
-//   // test('Should return the initial state', () => {
-//   //   let target = {type: 'EXPENSE_RESET', payload: state[0]}
-//   //   let testResult = expenseReducer(state, target);
-//   //   expect(testResult.length).toEqual(state.length);
-//   //   expect(testResult[0]).toBe(state[0]);
-//   //   expect(testResult[1]).toEqual(state[1]);
-//   // });
+  test('Should return a state with a new expense added', () => {
+    let newExpense = {title:
+      'bacon',
+      budget: 200,
+      id: 999,
+      published: new Date(),
+      categoryID: 1234
+
+    }
+    let target = {type: 'EXPENSE_CREATE', payload: newExpense}
+    let testResult = expenseReducer(state, target);
+    console.log('RESULT: ', testResult);
+    expect(testResult[1234].length).toEqual(2);
+    expect(testResult[1234][0]).toBe(state[1234][0]);
+    expect(testResult[1234][1]).toEqual(newExpense);
+  });
+  test('Should return a state with a modified target', () => {
+    let modExpense = state[1234][0]
+    modExpense.title = 'cake'
+    let target = {type: 'EXPENSE_UPDATE', payload: modExpense}
+    let testResult = expenseReducer(state, target)[1234];
+    expect(testResult.length).toEqual(1);
+    expect(testResult[0]).toBe(modExpense);
+  });
+  test('Should return the initial state', () => {
+    let target = {type: 'EXPENSE_RESET', payload: state[1234][0]}
+    let testResult = expenseReducer(state, target)[1234];
+    expect(testResult.length).toEqual(1);
+    expect(testResult[0]).toBe(state[1234][0]);
+  });
+
+  test('Should return a new category property with an empty array', () => {
+    let testCat = {
+      title: 'test',
+      budget: 2,
+      id: 666,
+      published: new Date(),
+    }
+    let target = {type: 'CATEGORY_CREATE', payload: state[1234][0]}
+    let testResult = expenseReducer(state, target)[666];
+    expect(testResult.length).toEqual(0);
+  });
+  test('Should return a set the category property to undefined', () => {
+
+    let target = {type: 'CATEGORY_DELETE', payload: state[1234][0]}
+    let testResult = expenseReducer(state, target)[1234];
+    expect(testResult).toEqual(undefined);
+  });
 });
